cfac3b5cd37f95bec3709ae29854239caf5fd55c
anidurg <102254578+anidurg@users.noreply.github.com>
2022-04-11 23:52:19 -0400

    test

---
+++ gitaindex.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+   <!-- <meta charset="UTF-8">
+    <script>
+
+
+        function showUser(str) {
+            if (str == "") {
+                document.getElementById("txtHint").innerHTML = "";
+                return;
+            }
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("txtHint").innerHTML = this.responseText;
+                }
+            }
+            xmlhttp.open("GET", "getchapter.php?q=" + str, true);
+            xmlhttp.send();
+        }
+    </script>
+       -->
+</head>
+<body>
+    <!--
+         <?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "anitamysql";
+        $dbname = "mydatabase";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        // mysql select query
+        $query = "SELECT * FROM gitamainlist";
+
+        $result1 = mysqli_query($conn, $query);
+
+        ?>
+
+        <?php
+    $txt = "W3Schools.com";
+    echo "I love $txt!";
+    ?>
+
+             -->
+    <h1>Hello</h1>
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love $txt!";
+    ?>
+    <!--
+
+
+    <form>
+
+        <select name="users" onchange="showUser(this.value)">
+            <option value="">Select a Chapter:</option>
+
+
+            <?php while($row1 = mysqli_fetch_array($result1)):;?>
+
+            <option value="<?php echo $row1[0];?>"><?php echo $row1[1];?></option>
+
+            <?php endwhile;?>
+            $conn -> close();
+        </select>
+    </form>
+    <br>
+    <div id="txtHint"><b>Chapter info will be listed here.</b></div>
+
+        -->
+
+</body>
+</html>
